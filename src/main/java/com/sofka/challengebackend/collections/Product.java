79db7ef928a4ac008b2b20676a466f381eee1b11
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-06-07 22:39:23 -0500

    Second collection created (Product)

--- src/main/java/com/sofka/challengebackend/collections/Product.java
+++ src/main/java/com/sofka/challengebackend/collections/Product.java
@@ -1,4 +1,115 @@
 package com.sofka.challengebackend.collections;
 
+
+import java.util.List;
+import java.util.Objects;
+
 public class Product {
+
+    private String productName;
+    private Long productPrice;
+    private String productDescription;
+    private Integer minAmount;
+    private Integer maxAmount;
+    private Integer availableAmount;
+    private List<Purveyor> purveyors;
+
+    public Product(String productName,
+                   Long productPrice,
+                   String productDescription,
+                   Integer minAmount,
+                   Integer maxAmount,
+                   Integer availableAmount,
+                   List<Purveyor> purveyors) {
+
+        this.productName = productName;
+        this.productPrice = productPrice;
+        this.productDescription = productDescription;
+        this.minAmount = minAmount;
+        this.maxAmount = maxAmount;
+        this.availableAmount = availableAmount;
+        this.purveyors = purveyors;
+    }
+
+    public String getProductName() {
+        return productName;
+    }
+
+    public void setProductName(String productName) {
+        this.productName = productName;
+    }
+
+    public Long getProductPrice() {
+        return productPrice;
+    }
+
+    public void setProductPrice(Long productPrice) {
+        this.productPrice = productPrice;
+    }
+
+    public String getProductDescription() {
+        return productDescription;
+    }
+
+    public void setProductDescription(String productDescription) {
+        this.productDescription = productDescription;
+    }
+
+    public Integer getMinAmount() {
+        return minAmount;
+    }
+
+    public void setMinAmount(Integer minAmount) {
+        this.minAmount = minAmount;
+    }
+
+    public Integer getMaxAmount() {
+        return maxAmount;
+    }
+
+    public void setMaxAmount(Integer maxAmount) {
+        this.maxAmount = maxAmount;
+    }
+
+    public Integer getAvailableAmount() {
+        return availableAmount;
+    }
+
+    public void setAvailableAmount(Integer availableAmount) {
+        this.availableAmount = availableAmount;
+    }
+
+    public List<Purveyor> getPurveyors() {
+        return purveyors;
+    }
+
+    public void setPurveyors(List<Purveyor> purveyors) {
+        this.purveyors = purveyors;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Product product = (Product) o;
+        return productName.equals(product.productName);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(productName);
+    }
+
+    @Override
+    public String toString() {
+        return "Product{" +
+                "productName='" + productName + '\'' +
+                ", productPrice=" + productPrice +
+                ", productDescription='" + productDescription + '\'' +
+                ", minAmount=" + minAmount +
+                ", maxAmount=" + maxAmount +
+                ", availableAmount=" + availableAmount +
+                ", purveyors=" + purveyors +
+                '}';
+    }
 }
