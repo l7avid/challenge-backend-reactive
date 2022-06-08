7c1ff3852bc416c6dac277ca0376284237230330
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-06-08 18:27:12 -0500

    Second usecase done (GetPurveyorByName)

--- src/main/java/com/sofka/challengebackend/repository/IPurveyorRepository.java
+++ src/main/java/com/sofka/challengebackend/repository/IPurveyorRepository.java
@@ -1,9 +1,13 @@
 package com.sofka.challengebackend.repository;
 
+import com.sofka.challengebackend.DTO.PurveyorDTO;
 import com.sofka.challengebackend.collections.Purveyor;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
 import org.springframework.stereotype.Repository;
+import reactor.core.publisher.Mono;
 
 @Repository
 public interface IPurveyorRepository extends ReactiveMongoRepository<Purveyor, String> {
+
+    Mono<Purveyor> findBypurveyorName(String purveyorName);
 }
--- src/main/java/com/sofka/challengebackend/usecases/GetPurveyorUseCase.java
+++ src/main/java/com/sofka/challengebackend/usecases/GetPurveyorUseCase.java
@@ -4,22 +4,20 @@ import com.sofka.challengebackend.DTO.PurveyorDTO;
 import com.sofka.challengebackend.mapper.PurveyorMapper;
 import com.sofka.challengebackend.repository.IPurveyorRepository;
 import org.springframework.stereotype.Service;
-import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
 
 @Service
-public class GetPurveyorsUseCase {
+public class GetPurveyorUseCase {
 
     private final IPurveyorRepository repository;
     private final PurveyorMapper mapper;
 
-
-    public GetPurveyorsUseCase(IPurveyorRepository repository, PurveyorMapper mapper) {
+    public GetPurveyorUseCase(IPurveyorRepository repository, PurveyorMapper mapper) {
         this.repository = repository;
         this.mapper = mapper;
     }
 
-    public Flux<PurveyorDTO> getAllPurveyors(){
-        //return repository.findAll().map(mapper::toPurveyorDTO);
-        return repository.findAll().map(purveyor -> mapper.toPurveyorDTO(purveyor));
+    public Mono<PurveyorDTO> getPurveyor(String purveyorName){
+        return repository.findBypurveyorName(purveyorName).map(purveyor -> mapper.toPurveyorDTO(purveyor));
     }
 }
