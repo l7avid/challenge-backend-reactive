ee7865fb352fd3eabeeff2c6f8229752ffcef15c
David Ernesto Reina Munar <davidereina@hotmail.com>
2022-06-08 21:40:16 -0500

    Route GetPurveyors implemented

--- src/main/java/com/sofka/challengebackend/routes/GetPurveyorsRoute.java
+++ src/main/java/com/sofka/challengebackend/routes/GetPurveyorsRoute.java
@@ -1,16 +1,24 @@
 package com.sofka.challengebackend.routes;
 
+import com.sofka.challengebackend.DTO.PurveyorDTO;
 import com.sofka.challengebackend.usecases.GetPurveyorsUseCase;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.web.reactive.function.BodyInserters;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import static org.springframework.web.reactive.function.server.RequestPredicates.*;
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
 
 @Configuration
 public class GetPurveyorsRoute {
 
     @Bean
     public RouterFunction<ServerResponse> allPurveyors(GetPurveyorsUseCase getPurveyorsUseCase){
-
+        return route(GET("/get/purveyors"), request -> ServerResponse.status(HttpStatus.OK)
+                .contentType(MediaType.APPLICATION_JSON)
+                .body(BodyInserters.fromProducer(getPurveyorsUseCase.getAllPurveyors(), PurveyorDTO.class)));
     }
 }
